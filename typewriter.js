e7abb6be7f2352e3d5e2ebced1589baaa8c131ff
Sean Oyler <xrysen@gmail.com>
2020-11-03 05:19:33 +0000

    Updated to pass eslint test

--- typewriter.js
+++ typewriter.js
@@ -2,13 +2,12 @@ const sentence = "hello there from lighthouse labs";
 
 let time = 0;
 
-for(let i = 0; i < sentence.length; i++) {
+for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
-    if(i === sentence.length - 1) {
+    if (i === sentence.length - 1) {
       process.stdout.write(sentence[i] + "\n");
-    }
-    else
+    } else
       process.stdout.write(sentence[i]);
   }, time);
   time += 50;
-};
+}
